9acd13d4cf386c448e1e972583699d8ed3dc7b3d
jlllyfish <james.chaigneaud@gmail.com>
2026-09-20 02:18:13 +0200

    moose

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ def api_send():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=int(os.environ.get("PORT", "5000")))
